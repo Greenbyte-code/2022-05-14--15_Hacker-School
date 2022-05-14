8eb8914578d6b7d058560ae202f06477391fe80d
Greenbyte <93258461+Greenbyte-code@users.noreply.github.com>
2022-05-14 14:48:36 +0000

    change main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-fileName = './/sys_test//sys_test_main.py'
+fileName = './/Jakob//Jakob_2.py'
 #.//Jan//Jan_2-1.py
 #.//Jakob//Jakob_1.py
 #.//Jan//Vergleich//Main.py
+#.//sys_test//sys_test_main.py
 
 with open(fileName, 'rb') as source_file:
   code = compile(source_file.read(), fileName, "exec")
